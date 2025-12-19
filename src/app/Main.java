e1004925dee8abb3b55cce73cd554ab2924193fc
antonn-tech <antonn.tech@gmail.com>
2025-12-19 20:14:04 +0200

    Fix: upload full code for sales calculation

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,29 @@
 package app;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Дані для товару №1
+        String product1 = "smartphone";
+        int days1 = 5;
+        double totalSales1 = 12153.41;
+        double dailySales1 = totalSales1 / days1;
+
+        // Виведення товару №1
+        System.out.printf("Product No 1: %s,%n", product1);
+        System.out.printf("total sales for %d days is EUR %.2f,%n", days1, totalSales1);
+        System.out.printf("sales by day is EUR %.2f.%n", dailySales1);
+
+        // Дані для товару №2
+        String product2 = "laptop";
+        int days2 = 7;
+        double totalSales2 = 10486.85;
+        double dailySales2 = totalSales2 / days2;
+
+        // Виведення товару №2
+        System.out.printf("Product No 2: %s,%n", product2);
+        System.out.printf("total sales for %d days is EUR %.2f,%n", days2, totalSales2);
+        System.out.printf("sales by day is EUR %.2f.%n", dailySales2);
+    }
 }
